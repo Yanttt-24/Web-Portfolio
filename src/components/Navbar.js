3d4764d182dc7d758c12439c66282207f13991c6
Yanting Jin <a745277901@gmail.com>
2025-10-05 16:09:07 +0100

    update navbar names test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -88,7 +88,7 @@ function NavBar() {
                 to="/resume"
                 onClick={() => updateExpanded(false)}
               >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+                <CgFileDocument style={{ marginBottom: "2px" }} /> AAA
               </Nav.Link>
             </Nav.Item>
 
@@ -98,7 +98,7 @@ function NavBar() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+                <ImBlog style={{ marginBottom: "2px" }} /> BBB
               </Nav.Link>
             </Nav.Item>
 
